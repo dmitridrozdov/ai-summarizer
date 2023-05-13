8402144ada64110d0731960b440ae938df5da9d3
dmitridrozdov <dm.drozdov@gmail.com>
2023-05-13 17:09:15 +1000

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,13 @@ import { logo } from "../assets";
 
 const Hero = () => {
   return (
-    <header className='w-full flex justify-center items-center flex-col'>
-      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
-
-        <button
+    <header className='w-full flex justify-center items-center'>
+      {/* <nav className='flex justify-between  w-full mb-5 pt-3'> */}
+        {/* <img src={logo} alt='sumz_logo' className='w-20' /> */}
+        <div className='head_text'>
+          Summarize Articles with OpenAI GPT-4
+        </div>
+        {/* <button
           type='button'
           onClick={() =>
             window.open("https://github.com/TidbitsJS/Summize", "_blank")
@@ -16,17 +18,17 @@ const Hero = () => {
           className='black_btn'
         >
           GitHub
-        </button>
-      </nav>
+        </button> */}
+      {/* </nav> */}
 
-      <h1 className='head_text'>
+      {/* <h1 className='head_text'>
         Summarize Articles with <br className='max-md:hidden' />
         <span className='orange_gradient '>OpenAI GPT-4</span>
       </h1>
       <h2 className='desc'>
         Simplify your reading with Summize, an open-source article summarizer
         that transforms lengthy articles into clear and concise summaries
-      </h2>
+      </h2> */}
     </header>
   );
 };
